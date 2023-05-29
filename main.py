4c0c32c13650367383edcd8359f82ec0e23eb79f
Ioannis Kolotouros <74195480+ioankolot@users.noreply.github.com>
2023-05-29 12:58:37 +0100

    added null space

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from brute_force import Brute_Force
 
 
 seed = 2
-number_of_qubits = 4
-steps = 30 #Choose number of steps to interpolate from initial to final Hamiltonian
+number_of_qubits = 6
+steps = 100 #Choose number of steps to interpolate from initial to final Hamiltonian
 connectivity = 'nearest-neighbors' #This is the connectivity of the non-parameterized gates in the Hardware Efficient ansatz
 single_qubit_gates = 'ry'
 entanglement_gates = 'cz'
@@ -22,5 +22,5 @@ problem = {'type':'MaxCut', 'properties': w}
 Brute_Force(problem)
 
 
-aqc_pqc = AQC_PQC(number_of_qubits, problem, steps, layers, single_qubit_gates, entanglement_gates, entanglement)
+aqc_pqc = AQC_PQC(number_of_qubits, problem, steps, layers, single_qubit_gates, entanglement_gates, entanglement, use_null_space=True)
 aqc_pqc.run()
